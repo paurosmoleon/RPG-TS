5b4a82c68db7aff4a18cc6829ca4add7242268bb
Pau <pauros2004@gmail.com>
2025-01-22 21:05:40 +0100

    feat(Jugador) Añadiendo la clase de jugador

--- src/model/Jugador.ts
+++ src/model/Jugador.ts
@@ -2,5 +2,25 @@ class Jugador {
     nombre:string
     puntos_salud: number
     puntos_ataque: number
-    
+    dinero: number
+
+    constructor(nombre:string){
+        this.nombre = nombre;
+        this.puntos_salud = 20
+        this.puntos_ataque = 0
+        this.dinero = 2
+    }
+
+    calculaFuerzaInicial(){
+        let fuerza_aleatoria = Math.floor(Math.random() * 15 )+ 1
+        this.puntos_ataque = fuerza_aleatoria
+    }
+
+
+    get getAtributos():string{
+        return `Nombre: ${this.nombre}\nSalud: ${this.puntos_salud}\nAtaque: ${this.puntos_ataque}\nDinero: ${this.dinero}`
+    }
 }
+
+
+export default Jugador
